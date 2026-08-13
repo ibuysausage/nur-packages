7fb72cf575acd2083008de75750442acd3b75e3b
ibuysausage <bannansmooth68@proton.me>
2026-08-13 14:40:38 -0400

    add metadata and ffmpeg as runtime dependency

--- pkgs/crdl/default.nix
+++ pkgs/crdl/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildGoModule, fetchFromGitHub, }:
+{ lib, stdenv, buildGoModule, fetchFromGitHub, makeWrapper, ffmpeg, }:
 
 buildGoModule rec {
   pname = "crunchyroll-downloader";
@@ -11,5 +11,20 @@ buildGoModule rec {
     sha256 = "sha256-pJG3gUakP6Jp+i05+MWsAd5OWN15KsxxWNdk0lh64zs=";
   };
 
+  nativeBuildInputs = [ makeWrapper ];
+
   vendorHash = "sha256-lNWzylzk/VT3/vpSlsxCnVCmUY24dn9zdOp+8TFo0yE=";
+
+  postFixup = '' 
+    wrapProgram $out/bin/crunchyroll-downloader \
+      --prefix PATH :  ${lib.makeBinPath [ ffmpeg ]}
+  '';
+
+  meta = with lib; {
+    description = "Downloads anime from Crunchyroll and outputs them in a MKV file";
+    homepage = "https://github.com/CuteTenshii/crunchyroll-downloader";
+    licenses = licenses.mit;
+    platforms = platforms.unix;
+  };
+
 }
